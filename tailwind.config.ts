aab514bcd3f139fee3127d3cf55486db1b0dc6f0
Carlos Eduardo <81780275+Carlos001-hash@users.noreply.github.com>
2025-03-25 18:15:41 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,14 +6,20 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
     extend: {
+      screens: {
+        sm: '480px',
+        ws: '510px',
+        md: '768px',
+        lg: '1024px',
+        xs: '1080px',
+        xl: '1280px',
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        animation: {
-          'spin-slow': 'spin 1s linear infinite', // Altere '3s' para a duração desejada
-        }
       },
     },
   },
