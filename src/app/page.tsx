ec28a573405ad36dfcc84c8596bb28a8b2d27083
Carlos Eduardo <81780275+Carlos001-hash@users.noreply.github.com>
2025-05-06 23:18:18 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 "use client"
 
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from 'framer-motion';
 import Image from "next/image";
-import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 export default function MyApp() {
@@ -19,6 +17,12 @@ export default function MyApp() {
   const [tr, setTr] = useState(Math.floor(Math.random() *3));
   const [switchBtn,setSwitchBtn] = useState(false);
   const [parar, setParar] = useState(true);
+  const [resultado, setResultado] = useState(0);
+  const [historicoResultados, setHistoricoResultados] = useState<React.ReactNode[]>([]);
+  const [acertos, setAcertos] = useState(0);
+  const [erros, setErros] = useState(0);
+  const [showM, setShowM] = useState(false);
+  const [opM, setOpM] = useState(0);
 
   const [isAnimating, setIsAnimating] = useState(false);
 
@@ -35,10 +39,18 @@ export default function MyApp() {
     let w = Math.floor(Math.random() * 100) + 1;
     let y = Math.floor(Math.random() *5)
 
-    let res = t + w;
+    let res: number[] = [] ;
 
-    let outroNumero = [res -5, res -10, res +2, res +1, res +10];
-    outroNumero[y] = res;
+    if (opM == 0) {
+      res[0] = t + w;
+    }else if (opM == 1) {
+      res[0] = t * w;
+    } else if (opM == 2) {
+      res[0] = w - t;
+    }
+
+    let outroNumero = [res[0] -5, res[0] -10, res[0] +2, res[0] +1, res[0] +10];
+    outroNumero[y] = res[0];
     setDdd(outroNumero);
 
     const mt = [t];
@@ -52,11 +64,10 @@ export default function MyApp() {
       setNumero2(mt)
     };
 
-    setSoma(res);
+    setSoma(res[0]);
     setTr(Math.floor(Math.random() *3));
     setSwitchBtn(true);
   };
-
 
   if (tr == 1) {
     console.log("one")
@@ -87,7 +98,37 @@ export default function MyApp() {
   for (let i = 0; i <= ellipse -1; i++ ) {
     elli.push(<Image src="/Ellipse.svg" width={336} height={336} alt="" className="w-9 md:w-10 xs:w-9 xl:w-11" />)
   }
-  
+
+  let mto: React.ReactNode[] = [];
+const handlePonts = () => {
+  let mps: number[] = [];
+  let text = `${numero1} + ${numero2} = ${resultado}`
+  if (resultado == soma) {
+    mto.push(<p className="text-green-400 font-semibold">{`${text}`}</p>)
+   } else {
+    mto.push(<p className="text-red-400 font-semibold">{`${text}`}</p>)
+   }
+}
+
+
+
+
+const adicionarResultado = (numero1: number, numero2: number, resultado: number, soma: number) => {
+  const text = `${numero1} + ${numero2} = ${resultado}`;
+  setResultado(resultado);
+  setHistoricoResultados(prevResultados => [
+    
+      
+      <p key={prevResultados.length} className={resultado === soma ? `text-green-400 font-semibold ${setAcertos(acertos +1)}` : `text-red-400 font-semibold ${setErros(erros +1)}`}>
+          {text}
+      </p>,
+      ...prevResultados,
+  ]);
+};
+
+
+
+
     return (
       <div className="w-full h-screen overflow-auto">
         {parar  || (
@@ -97,15 +138,15 @@ export default function MyApp() {
               <div className=" w-full h-auto flex flex-col gap-2">
                 <div className="flex flex-row">
                   <Image src="/Polygon.svg" width={310} height={310} alt="" className="w-12" />
-                  <div className="relative w-full h-12 flex flex-row items-start ml-2  bg-gray-200 rounded-full border-2 border-black pl-2">{poly}</div>  
+                  <div className="relative w-full h-12 flex flex-row items-start ml-2  bg-gray-200 rounded-full border-2 border-black pl-2 pt-1.5">{poly}</div>  
                 </div>
                 <div className="flex flex-row">
                   <Image src="/Rectangle.svg" width={310} height={310} alt="" className="w-12" />
-                  <div className="relative w-full h-12 flex flex-row items-start ml-2  bg-gray-200 rounded-full border-2 border-black pl-2">{rect}</div>  
+                  <div className="relative w-full h-12 flex flex-row items-start ml-2  bg-gray-200 rounded-full border-2 border-black pl-2 pt-1">{rect}</div>  
                 </div>
                 <div className="flex flex-row">
                   <Image src="/Ellipse.svg" width={310} height={310} alt="" className="w-12" />
-                  <div className="relative w-full h-12 flex flex-row items-start ml-2  bg-gray-200 rounded-full border-2 border-black pl-2">{elli}</div>  
+                  <div className="relative w-full h-12 flex flex-row items-start ml-2  bg-gray-200 rounded-full border-2 border-black pl-2 pt-1">{elli}</div>  
                 </div>
               </div>
               <div className="w-full h-14 flex flex-row items-center justify-evenly">
@@ -154,7 +195,9 @@ export default function MyApp() {
             transition={{ type: 'spring', duration: 1, delay: 1 }}
             className=" w-auto h-auto flex  flex-row gap-4 ws:gap-6 mt-5">
             <div className="size-[110px]  xl:size-[160px] ws:size-[140px] flex justify-center items-center bg-amber-50 rounded-md cursor-default  text-6xl ws:text-[76px] xl:text-[86px] font-semibold border-2 border-black  drop-shadow-lg">{numero1}</div>
-            <Image src="/Mais.svg"  width={32} height={32} alt="" className="w-7 ws:w-20"/>
+            {opM == 0 ? (<button onClick={() => {setOpM(opM +1)}}><Image src="/Mais.svg"  width={32} height={32} alt="" className="w-7 ws:w-20"/></button>) : null}
+            {opM == 1 ? (<button onClick={() => {setOpM(opM +1)}}><Image src="/Vezes.svg"  width={32} height={32} alt="" className="w-7 ws:w-20"/></button>) : null}
+            {opM == 2 ? (<button onClick={() => {setOpM(opM -2)}}><Image src="/Menos.svg"  width={32} height={32} alt="" className="w-7 ws:w-20"/></button>) : null}
             <div className="size-[110px] xl:size-[160px] ws:size-[140px] flex justify-center items-center bg-amber-50 rounded-md cursor-default text-6xl ws:text-[76px] xl:text-[86px] font-semibold border-2 border-black drop-shadow-lg">{numero2}</div>
           </motion.div>
 
@@ -172,7 +215,7 @@ export default function MyApp() {
               initial={{ y: '-100vh',}}
               animate={{ y: isAnimating ? 228 : '-100vh' }}
               transition={{ type: 'tween', duration: 0.7 }}
-              className="absolute bottom-[280px] right-[125px] z-50">
+              className="absolute bottom-[350px] right-[125px] z-50">
                 <Image src="/Fechado.svg" width={83} height={155} alt="" className="w-48 flex z-50 "/>
               </motion.div>
             )}
@@ -180,7 +223,7 @@ export default function MyApp() {
                 initial={{ y: '-100vh',}}
                 animate={{ y: isAnimating ? 228 : '-100vh' }}
                 transition={{ type: 'tween', duration: 0.7 }}
-                className=" group absolute w-48 h-auto bottom-[280px] right-[125px] z-50">
+                className=" group absolute w-48 h-auto bottom-[350px] right-[125px] z-50">
                   
                 
               {count == 3 && (
@@ -210,17 +253,42 @@ export default function MyApp() {
             </motion.div>
           
         </div>
-        <div className="w-full h-64 flex justify-center">
+        <div className="relative w-full h-64 flex justify-center">
+        <motion.div
+          initial={{ x: '-150vh' }}
+          animate={{ x: isAnimating ? 0 : '-100vh' }}
+          transition={{ type: 'tween', duration: 1, delay: 0.8, stiffness: 50, damping: 6  }}
+          className="absolute left-[0px] w-auto h-64 flex justify-start items-center  bg-gay-100/70 rounded-full drop-shadow-sm z-50">
+            <div className={ showM ? "flex  justify-center items-center w-[256px] h-[160px]  bg-yellow-50 border-2 border-black rounded-lg z-50" : "hidden  justify-center items-centerw-[256px] h-[160px]  bg-yellow-50 border-2 border-black rounded-lg z-50"}>
+              <div className="w-[234px] h-[138px] flex flex-col bg-black border-2 border-black rounded-lg overflow-y-auto p-2">
+                <div className="w-full h-auto p-1">
+                  <h3 className="text-green-400 font-semibold">Erros e acertos</h3>
+                  <p className="text-green-400 font-semibold">Acertos: {acertos}</p>
+                  <p className="text-green-400 font-semibold">Erros: {erros}</p>
+                  <p className="text-green-400 font-semibold">----------------------------------</p>
+                </div>
+                {historicoResultados.map((item, index) => (
+                  <div key={index} className="w-full h-auto flex flex-col">
+                   {item}
+                  </div>
+                ))}
+              </div>
+            </div>
+            <button onClick={() => {showM ? setShowM(false) : setShowM(true)}} className="relative w-16 h-64 flex items-center bg-red-500y -ml-1">
+                <Image src="/grip.svg" width={140} height={194}  alt="" className="-ml-3" />
+            </button>
+        </motion.div>
+
           <motion.div
           initial={{ x: '-150vh' }}
           animate={{ x: isAnimating ? 0 : '-100vh' }}
           transition={{ type: 'spring', duration: 1, delay: 0.8, stiffness: 50, damping: 6  }}
-          className="w-64 h-64 flex justify-center items-center  bg-gay-100/70 rounded-full drop-shadow-sm">
-              <button onClick={() => {if(ddd[0] == soma){setCount(count +1) ;handleClickMath(); console.log(count)}else if (count > 0) {setCount(count -1); console.log(count)}}} className="size-12 bg-red-400/70  mt-16 rounded-full border-2 border-black drop-shadow-lg text-2xl xs:hover:translate-y-2 xs:hover:scale-125  hover:border-white cursor-default">{ddd[0]}</button>
-              <button onClick={() => {if(ddd[1] == soma){setCount(count +1) ;handleClickMath(); console.log(count)}else if (count > 0) {setCount(count -1); console.log(count)}}} className="size-12 bg-red-400/70  mb-mt-16 rounded-full border-2 border-black drop-shadow-lg text-2xl xs:hover:-translate-y-2 xs:hover:scale-125  hover:border-white cursor-default">{ddd[1]}</button>
-              <button onClick={() => {if(ddd[2] == soma){setCount(count +1) ;handleClickMath(); console.log(count)}else if (count > 0) {setCount(count -1); console.log(count)}}} className="size-12 bg-red-400/70  mt-16 rounded-full border-2 border-black drop-shadow-lg text-2xl xs:hover:translate-y-2 xs:hover:scale-125  hover:border-white cursor-default">{ddd[2]}</button>
-              <button onClick={() => {if(ddd[3] == soma){setCount(count +1) ;handleClickMath(); console.log(count)}else if (count > 0) {setCount(count -1); console.log(count)}}} className="size-12 bg-red-400/70  mb-mt-16 rounded-full border-2 border-black drop-shadow-lg text-2xl xs:hover:-translate-y-2 xs:hover:scale-125  hover:border-white cursor-default">{ddd[3]}</button>
-              <button onClick={() => {if(ddd[4] == soma){setCount(count +1) ;handleClickMath(); console.log(count)}else if (count > 0) {setCount(count -1); console.log(count)}}} className="size-12 bg-red-400/70  mt-16 rounded-full border-2 border-black drop-shadow-lg text-2xl xs:hover:translate-y-2 xs:hover:scale-125  hover:border-white cursor-default">{ddd[4]}</button>
+          className="w-[286px] h-64 flex justify-center items-center  bg-gay-100/70 rounded-full drop-shadow-sm">
+              <button onClick={() => {if (ddd[0] == soma) {setCount(count + 1); handleClickMath();}adicionarResultado(numero1[0], numero2[0], ddd[0], soma);}} className="size-14 bg-red-400/70  mt-16 rounded-full border-2 border-black drop-shadow-lg text-2xl xs:hover:translate-y-2 xs:hover:scale-125  hover:border-white cursor-default">{ddd[0]}</button>
+              <button onClick={() => {if (ddd[1] == soma) {setCount(count + 1); handleClickMath();}adicionarResultado(numero1[0], numero2[0], ddd[1], soma);}} className="size-14 bg-red-400/70  mb-mt-16 rounded-full border-2 border-black drop-shadow-lg text-2xl xs:hover:-translate-y-2 xs:hover:scale-125  hover:border-white cursor-default">{ddd[1]}</button>
+              <button onClick={() => {if (ddd[2] == soma) {setCount(count + 1); handleClickMath();}adicionarResultado(numero1[0], numero2[0], ddd[2], soma);}} className="size-14 bg-red-400/70  mt-16 rounded-full border-2 border-black drop-shadow-lg text-2xl xs:hover:translate-y-2 xs:hover:scale-125  hover:border-white cursor-default">{ddd[2]}</button>
+              <button onClick={() => {if (ddd[3] == soma) {setCount(count + 1); handleClickMath();}adicionarResultado(numero1[0], numero2[0], ddd[3], soma);}} className="size-14 bg-red-400/70  mb-mt-16 rounded-full border-2 border-black drop-shadow-lg text-2xl xs:hover:-translate-y-2 xs:hover:scale-125  hover:border-white cursor-default">{ddd[3]}</button>
+              <button onClick={() => {if (ddd[4] == soma) {setCount(count + 1); handleClickMath();}adicionarResultado(numero1[0], numero2[0], ddd[4], soma);}} className="size-14 bg-red-400/70  mt-16 rounded-full border-2 border-black drop-shadow-lg text-2xl xs:hover:translate-y-2 xs:hover:scale-125  hover:border-white cursor-default">{ddd[4]}</button>
           </motion.div>
         </div>
         <motion.div
